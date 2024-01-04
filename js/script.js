ffcd30ad73c8b1f990dff014e3114e50ee200d32
Kaveenxz <kaveenhansithx@gmail.com>
2024-01-04 12:14:16 +0530

    make the animation with js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,11 @@ function toggleVideo(){
     video.pause();
     trailer.classList.toggle('active')
 }
+function changeBg(bg, title){
+    const banner = document.querySelector(".banner")
+    const contents = document.querySelectorAll('.content')
+    banner.style.background = `url("../assets/images/movies/${bg}")`;
+    banner.style.backgroundSize = 'cover';
+    banner.style.backgroundPosition = 'center';
+
+}
